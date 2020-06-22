4521ac87e88093d4d59b81266e197374d82c6809
Kamil <kamil.stus@gmail.com>
2020-06-22 19:24:25 +0200

    New service script - optimization and comments

--- TerraformECSFargate/new-service.sh
+++ TerraformECSFargate/new-service.sh
@@ -4,19 +4,25 @@ if test -z $1
 then
   echo "You must specify TFVARS service definition file. For example \"./new-service.sh services/customer-service.tfvars"
 else
-  terraform output > newservice.tfvars
-  cat newservice.tfvars | sed 's/= */= "/' | sed 's/$/"/' | sed 's/"\["/\[/' | sed 's/\]"/\]/' | sed 's/,"/,/' > terraform.tfvars
-  rm newservice.tfvars
-
   dir="svc_"`echo $1 | awk -F'/' '{print $NF}' | cut -d "." -f 1`
   file=`echo $1 | awk -F'/' '{print $NF}'`
-  mkdir $dir
+
+# Test if service directory exists. If so, delete the content
+  if test -d $dir
+  then
+    echo "Service directory exists. Deleting ..."
+    rm $dir/*.*
+  else
+    mkdir $dir
+  fi
+
+# Prepare new service directory (all *.tf files, tfvars from existing environment and service definition)
+  terraform output | sed 's/= */= "/' | sed 's/$/"/' | sed 's/"\["/\[/' | sed 's/\]"/\]/' | sed 's/,"/,/' > $dir/terraform.tfvars
+  cp new-service.tf/*.* $dir
+  cp $1 $dir
+
+# Run terraform init and apply
   cd $dir
-
-  cp ../new-service.tf/*.* .
-  cp ../$1 .
-  mv ../terraform.tfvars .
-
   terraform init
   terraform apply -var-file="$file"
 fi
